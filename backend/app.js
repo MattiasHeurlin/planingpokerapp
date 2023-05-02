733661a1ab0f449e83a70eb881db1a8129ae4d9b
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-02 16:42:44 +0200

    Initial setup of project
    Co-authored by: Mattias Heurlin <MattiasHeurlin@users.noreply.github.com>, Sandra Stridsman <zandrastr@users.noreply.github.com>, Sofia Olsson <Sofilus@users.noreply.github.com>

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const options = {
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(process.env.DB_URI, options)
+  .then(() => console.log('Connected to Database'))
+  .catch((err) => console.error('Error connecting to database: ', err));
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
