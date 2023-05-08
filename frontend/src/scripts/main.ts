735407abf5cf09542800712b0663b209aaeee0a7
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-08 13:43:15 +0200

    added socket for monitoring and re-rendering rooms

--- frontend/src/scripts/main.ts
+++ frontend/src/scripts/main.ts
@@ -1,5 +1,5 @@
 import '../style.css';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import { getAllRooms } from './roomSelection';
 import { renderUserView } from './userView';
 import { Room } from './roomSelection';
@@ -13,31 +13,33 @@ app!.innerHTML = `
     <section class="previous-topics"><h2>Previous topics<h2></section>
     <footer class="footer"></footer>
   </div>
-`
-
+`;
 
 export const socket = io('http://localhost:3000');
 
-socket.emit("test");
+socket.emit('test');
 
-socket.on("test", (arg) => {
-  console.log(arg)
-})
+socket.on('test', (arg) => {
+  console.log(arg);
+});
+
+socket.on('monitorRooms', () => {
+  getAllRooms();
+});
+
 function init(): void {
   getAllRooms();
 }
 
-socket.on("userAlreadyInRoom", (data) => {
+socket.on('userAlreadyInRoom', (data) => {
   console.log(data);
-  const error = document.createElement("p");
-  error.innerText = "Namnet är upptaget, välj ett annat";
+  const error = document.createElement('p');
+  error.innerText = 'Namnet är upptaget, välj ett annat';
   app!.append(error);
 });
-socket.on("joinRoom", (room: Room) => {
-  console.log(room)
+socket.on('joinRoom', (room: Room) => {
+  console.log(room);
   renderUserView(room);
-  
-  
-})
+});
 
 init();
