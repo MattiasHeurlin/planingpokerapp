90f1eeb95654a75e04780c93d6ec3bb1537aa4f9
Sofilus <sofia00olsson@gmail.com>
2023-05-08 12:26:01 +0200

    Moved elements for creating new topic to a own function

--- frontend/src/scripts/adminView.ts
+++ frontend/src/scripts/adminView.ts
@@ -1,19 +1,7 @@
 const adminContainer = document.querySelector('#app') as HTMLDivElement;
 
 export function printAdminView (){
-    const addNewTopicContainer = document.createElement('div') as HTMLDivElement;
-    const addTopicTitle = document.createElement('p') as HTMLParagraphElement;
-    const addTopicInput = document.createElement('input') as HTMLInputElement;
-    const addNewTopicBtn = document.createElement('button') as HTMLButtonElement;
-
-    addTopicTitle.innerText = 'Lägg till topic';
-    addTopicInput.placeholder = 'Ny topic';
-    addNewTopicBtn.innerText = 'Lägg till';
-
-    adminContainer.appendChild(addNewTopicContainer);
-    addNewTopicContainer.appendChild(addTopicTitle);
-    addNewTopicContainer.appendChild(addTopicInput);
-    addNewTopicContainer.appendChild(addNewTopicBtn);
+    createdAddNewTopic();
 
     const upcomingTopicsContainer = document.createElement('div') as HTMLDivElement;
     const upcomingTopicsTitle = document.createElement('h3') as HTMLHeadingElement;
@@ -37,6 +25,22 @@ export function printAdminView (){
     topicContainer.appendChild(moveTopicUpBtn);
     topicContainer.appendChild(moveTopicDownBtn);
     
+}
+
+function createdAddNewTopic(){
+    const addNewTopicContainer = document.createElement('div') as HTMLDivElement;
+    const addTopicTitle = document.createElement('p') as HTMLParagraphElement;
+    const addTopicInput = document.createElement('input') as HTMLInputElement;
+    const addNewTopicBtn = document.createElement('button') as HTMLButtonElement;
+
+    addTopicTitle.innerText = 'Lägg till topic';
+    addTopicInput.placeholder = 'Ny topic';
+    addNewTopicBtn.innerText = 'Lägg till';
+
+    adminContainer.appendChild(addNewTopicContainer);
+    addNewTopicContainer.appendChild(addTopicTitle);
+    addNewTopicContainer.appendChild(addTopicInput);
+    addNewTopicContainer.appendChild(addNewTopicBtn);
 }
 
 
