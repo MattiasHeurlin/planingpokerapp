76b67104b467ca44a4218534428aafc22cae5ea4
Sofilus <sofia00olsson@gmail.com>
2023-05-08 12:24:25 +0200

    Created elements for upcoming topics

--- frontend/src/scripts/adminView.ts
+++ frontend/src/scripts/adminView.ts
@@ -14,6 +14,29 @@ export function printAdminView (){
     addNewTopicContainer.appendChild(addTopicTitle);
     addNewTopicContainer.appendChild(addTopicInput);
     addNewTopicContainer.appendChild(addNewTopicBtn);
+
+    const upcomingTopicsContainer = document.createElement('div') as HTMLDivElement;
+    const upcomingTopicsTitle = document.createElement('h3') as HTMLHeadingElement;
+    const topicContainer = document.createElement('div') as HTMLDivElement;
+    const removeUpcomingTopicBtn = document.createElement('button') as HTMLButtonElement;
+    const upcomingTopic = document.createElement('p') as HTMLParagraphElement;
+    const moveTopicUpBtn = document.createElement('button') as HTMLButtonElement; 
+    const moveTopicDownBtn = document.createElement('button') as HTMLButtonElement;
+    
+    upcomingTopicsTitle.innerText = 'Kommande topics';
+    removeUpcomingTopicBtn.innerText = '-';
+    upcomingTopic.innerText =  'Test';
+    moveTopicDownBtn.innerText = 'Ner';
+    moveTopicUpBtn.innerText = 'Upp';
+
+    adminContainer.appendChild(upcomingTopicsContainer);
+    upcomingTopicsContainer.appendChild(upcomingTopicsTitle);
+    upcomingTopicsContainer.appendChild(topicContainer);
+    topicContainer.appendChild(removeUpcomingTopicBtn);
+    topicContainer.appendChild(upcomingTopic);
+    topicContainer.appendChild(moveTopicUpBtn);
+    topicContainer.appendChild(moveTopicDownBtn);
+    
 }
 
 
