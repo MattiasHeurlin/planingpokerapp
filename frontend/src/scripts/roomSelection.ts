1188d674195a90dc4809a01253ab503bb22de5a9
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-08 13:29:03 +0200

    added elements for create room in roomselection

--- frontend/src/scripts/roomSelection.ts
+++ frontend/src/scripts/roomSelection.ts
@@ -1,7 +1,5 @@
-import { socket } from "./main";
-
-
-
+import { createRoomElements } from './createRoom';
+import { socket } from './main';
 
 export interface Room {
   admin: string;
@@ -9,6 +7,7 @@ export interface Room {
   usersWhoLeft: string[];
   upcomingTopics: Topic[];
   currentTopic: Topic[];
+  finishedTopics: Topic[];
 }
 export interface User {
   name: string;
@@ -19,44 +18,45 @@ export interface Topic {
   score?: number;
 }
 export function getAllRooms() {
-  fetch("http://localhost:3000/rooms")
+  fetch('http://localhost:3000/rooms')
     .then((res) => res.json())
     .then((data) => {
       if (data.length === 0) {
-        console.log("No rooms found");
+        console.log('No rooms found');
       }
       renderRooms(data);
     });
 }
 
 export function renderRooms(rooms: Room[]) {
-  const div = document.createElement("div");
-  div.classList.add("room-select-container");
-  const main = document.querySelector<HTMLDivElement>(".main-content");
-  const adminText = document.createElement("p");
-  adminText.innerText = "Rum Admin:";
+  const div = document.createElement('div');
+  div.classList.add('room-select-container');
+  const main = document.querySelector<HTMLDivElement>('.main-content');
+  const adminText = document.createElement('p');
+  adminText.innerText = 'Rum Admin:';
 
   for (let i = 0; i < rooms.length; i++) {
     const room = rooms[i];
-    const roomDiv = document.createElement("div");
-    roomDiv.classList.add("room");
-    const inputContainer = document.createElement("div");
-    const roomName = document.createElement("h2");
+    const roomDiv = document.createElement('div');
+    roomDiv.classList.add('room');
+    const inputContainer = document.createElement('div');
+    const roomName = document.createElement('h2');
     roomName.innerText = room.admin;
-    const input = document.createElement("input");
-    input.type = "text";
-    input.placeholder = 'Skriv in ditt namn'
-    const button = document.createElement("button");
-    button.innerText = "Gå med";
-    button.addEventListener("click", () => {
-      
-      socket.emit("joinRoom", {name: input.value, roomIndex: i, });
-      console.log(input.value, i)
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Skriv in ditt namn';
+    const button = document.createElement('button');
+    button.innerText = 'Gå med';
+    button.addEventListener('click', () => {
+      socket.emit('joinRoom', { name: input.value, roomIndex: i });
+      console.log(input.value, i);
     });
 
     inputContainer.append(input, button);
-    roomDiv.append(adminText ,roomName, inputContainer);
+    roomDiv.append(adminText, roomName, inputContainer);
     div.append(roomDiv);
-  };
+  }
+
   main!.append(div);
+  createRoomElements();
 }
