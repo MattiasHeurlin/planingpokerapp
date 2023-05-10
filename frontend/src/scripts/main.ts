fd7be7e2a2495034d0620b5d782e91e8e0248fab
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-05-10 17:56:27 +0200

    created added separate socket funcs for admin/user

--- frontend/src/scripts/main.ts
+++ frontend/src/scripts/main.ts
@@ -2,7 +2,12 @@ import '../style.css';
 import { io } from 'socket.io-client';
 import { getAllRooms } from './roomSelection';
 
-import { addVote, renderRunningRoom, renderUserView } from './userView';
+import {
+  addVote,
+  renderComingTopics,
+  renderRunningRoom,
+  renderUserView,
+} from './userView';
 
 import { superadminLogin } from './superadminLogin';
 import { Room } from './roomSelection';
@@ -23,75 +28,11 @@ app!.innerHTML = `
 export const socket = io('http://localhost:3000');
 
 function init(): void {
-  addSockets();
   getAllRooms();
   superadminLogin();
-}
-
-function addSockets() {
-  socket.on('changeTopicOrderAdmin', (room) => {
-    printAdminView(room);
-  });
-
-  socket.on('removeTopicAdmin', (room) => {
-    printAdminView(room);
-  });
-
-  socket.on('addTopicAdmin', (room) => {
-    printAdminView(room);
-  });
-
-  socket.on('userAlreadyInRoom', (data) => {
-    console.log(data);
-    const error = document.createElement('p');
-    error.innerText = 'Namnet är upptaget, välj ett annat';
-    app!.append(error);
-  });
-
-  socket.on('joinRoom', (room: Room) => {
-    console.log(room);
-    // renderUserView(room); Går igång direkt förtillfället :FIXME
-    renderRunningRoom(room);
-  });
 
   socket.on('monitorRooms', () => {
     getAllRooms();
-  });
-
-  socket.on('monitorRoom', (room: Room) => {
-    console.log(room);
-    renderRunningRoom(room);
-  });
-
-  socket.on('startGame', (room) => {
-    // lägg till rendera nästa fråga funktion här (user-vy)
-    console.log(room);
-  });
-
-  socket.on('startGameAdmin', (room) => {
-    // lägg till rendera nästa fråga funktion här (admin-vy)
-    console.log(room);
-  });
-
-  socket.on('vote', (room: Room) => {
-    console.log(room);
-    addVote(room, false);
-  });
-
-  socket.on('allVoted', (room: Room) => {
-    addVote(room, true);
-  });
-
-  socket.on('noTopics', () => {
-    console.log('You need to add atleast 1 topic to start the game.');
-  });
-
-  socket.on('missingVotes', () => {
-    console.log("Everyone hasn't finished voting yet.");
-  });
-
-  socket.on('endSession', (room) => {
-    renderEndSessionPage(room);
   });
 }
 
